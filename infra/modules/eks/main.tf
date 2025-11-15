04783d2659139203d3ad912b29983387b4d74b10
sofiafernandezmoreno <chui274@gmail.com>
2025-11-15 11:31:08 +0100

    Include new charts

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -16,7 +16,8 @@ module "eks" {
       max_size     = 3
       desired_size = 1
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
     }
   }
 
